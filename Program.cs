2787afaa96f3f41d639259dedabdeef54ea363a1
Ryaza5562 <ryaza5562@gmail.com>
2023-07-27 11:20:05 +0300

    Usage with args in cmd

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@
         for (int k = 1; k <= n; k++)
         {
             if (k < 0 || k > n)
-                throw new ArgumentException("Неверные аргументы");
+                throw new ArgumentException("Bad email");
 
             int numerator = 1;
             int denominator = 1;
@@ -22,7 +22,7 @@
 
             sum += (numerator / denominator);
         }
-        return sum+1;
+        return sum + 1;
     }
 
     static List<string> SumCombinations(string emailName)
@@ -44,47 +44,66 @@
         return result;
     }
 
+    static void WriteInFile(string pathToSave, List<string> nameEmail, string domainEmail, string password)
+    {
+        using (StreamWriter sw = new StreamWriter(File.Create(pathToSave + "Emails.txt")))
+        {
+            for (int i = 0; i < nameEmail.Count; i++)
+            {
+                if (password != "")
+                    sw.Write(nameEmail[i] + domainEmail + ":" + password);
+                else
+                    sw.Write(nameEmail[i] + domainEmail);
+
+                if (nameEmail.Count - i != 1)
+                    sw.Write("\n");
+            }
+        }
+    }
+
     static void Main(string[] args)
     {
+        string email = "";
+        string password = "";
+        bool cmdUsage = false;
+        if (args.Length > 0)
+        {
+            cmdUsage = true;
+            if (args.Length > 0)
+                email = args[0];
+            if (args.Length == 2)
+                password = args[1];
+        }
         try
         {
-            Console.WriteLine("Generator subemails by Ryaza");
-            Console.Write("Enter email: ");
-            string email = Console.ReadLine();
-            Console.WriteLine("Add password after emails(Yes/DEFAULT NO): ");
-            string answer = Console.ReadLine();
-            string password = "";
-            if (answer.ToLower() == "yes")
+            if (!cmdUsage)
             {
-                Console.Write("Enter password: ");
-                password = Console.ReadLine();
-            }
-
-            if (email != null || email != "")
-            {
-                int combinationResult = CalculateAmountCombinations(email);
-                Console.WriteLine("Amount generating subemails: " + combinationResult);
-            }
-            else
-                throw new ArgumentException();
-            List<string> result = SumCombinations(email.Substring(0, email.IndexOf('@')));
-            using (StreamWriter sw = new StreamWriter(File.Create("Emails.txt")))
-            {
-                for(int i = 0; i < result.Count; i++)
+                Console.WriteLine("Generator subemails by Ryaza");
+                Console.Write("Enter email: ");
+                email = Console.ReadLine();
+                Console.WriteLine("Add password after emails(Yes/DEFAULT NO): ");
+                string answer = Console.ReadLine();
+                if (answer.ToLower() == "yes")
                 {
-                    if (answer.ToLower() == "yes")
-                        sw.Write(result[i] + email.Substring(email.IndexOf('@')) + ":" + password);
-                    else
-                        sw.Write(result[i] + email.Substring(email.IndexOf('@')));
-
-                    if (result.Count - i != 1)
-                        sw.Write("\n");
+                    Console.Write("Enter password: ");
+                    password = Console.ReadLine();
                 }
+
+                if (email != null || email != "")
+                {
+                    int combinationResult = CalculateAmountCombinations(email);
+                    Console.WriteLine("Amount generating subemails: " + combinationResult);
+                }
+                else
+                    throw new Exception();
             }
+            List<string> result = SumCombinations(email.Substring(0, email.IndexOf('@')));
+            WriteInFile(AppDomain.CurrentDomain.BaseDirectory, result, email.Substring(email.IndexOf('@')), password);
             Console.WriteLine("Subemails generated in Email.txt file!");
-            Console.ReadKey();
+            if (!cmdUsage)
+                Console.ReadKey();
         }
-        catch (ArgumentException ex)
+        catch (Exception ex)
         {
             Console.WriteLine(ex.Message);
         }
